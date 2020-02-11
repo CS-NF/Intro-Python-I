0a23022ad18d751f441389e745f4b6102f17dd9a
Noah Franco <noahfranco@jonadabs-mini.lan>
2020-02-11 18:18:16 -0500

    finished classes.py

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -11,9 +11,27 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 
 # YOUR CODE HERE
 
+import os
+
+# need to find the file path
+path = os.path.dirname(os.path.realpath(__file__))
+
+f = open(f"{path}/foo.txt", "r")
+read_the_data = f.read()
+print(read_the_data)
+
+
+
+
+
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+
+bar_f = open(f"{path}/bar.txt", "w") 
+bar_f.write(str("To infinity and beyond\n Galaxy far far away\n Live long and prosper"))
+
+bar_f.close()
